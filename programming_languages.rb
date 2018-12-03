bb7962a62b20ff898622ea99b7973b7add478e90
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-12-03 19:17:16 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,9 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |type, language|
+    if type == :oo 
+      language.each do |name, info|
+        new_hash[name] = style
+        new_hash[name] = 
 end
